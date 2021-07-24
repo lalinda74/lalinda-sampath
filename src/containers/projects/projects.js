c60e74bcd0e31f6f0206958fb6a0de2b0d697447
lalinda74 <diaslalinda@gmail.com>
2021-07-24 20:34:48 +0530

    adding responsive images

--- src/containers/projects/projects.js
+++ src/containers/projects/projects.js
@@ -1,5 +1,6 @@
 import './projects.scss';
-import Poster1 from  '../../assets/images/poster1.png';
+import Poster1x from  '../../assets/images/poster1-1x.png';
+import Poster2x from  '../../assets/images/poster1-2x.png';
 import Poster2 from  '../../assets/images/poster2.jpeg';
 import Poster3 from  '../../assets/images/poster3.jpeg';
 
@@ -14,13 +15,13 @@ function Projects() {
         </div>
         <div className="slider">
           <div className="p-banner">
-            <img src= {Poster1} alt = "banner 1" srcSet={`${Poster1} 1x, ${Poster2} 2x`} />
+            <img src= {Poster1x} srcSet={`${Poster1x} 1x, ${Poster2x} 2x`} alt = "banner 1" />
           </div>
           <div className="p-banner">
-            <img src= {Poster2} alt = "banner 2" />
+            <img src= {Poster2} srcSet={`${Poster2} 1x, ${Poster2} 2x`} alt = "banner 2" />
           </div>
           <div className="p-banner">
-            <img src= {Poster3} alt = "banner 3" />
+            <img src= {Poster3} srcSet={`${Poster3} 1x, ${Poster3} 2x`} alt = "banner 3" />
           </div>
         </div>
       </div>
