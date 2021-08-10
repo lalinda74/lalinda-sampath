f5457e7740e4da0bf945eaa82dd0addf08a417b8
lalinda74 <diaslalinda@gmail.com>
2021-08-10 23:12:46 +0530

    UI changes

--- src/containers/stories/stories.js
+++ src/containers/stories/stories.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './stories.scss';
 import Article from '../../components/article/article';
-import Image from '../../assets/images/article1.png';
+import Article1 from '../../assets/images/article1.png';
+import Article2 from '../../assets/images/article2.png';
 
 function useOnScreen(options) {
     const [ref, setRef] = React.useState(null);
@@ -33,10 +34,10 @@ function Stories() {
     const [setRef, visible] = useOnScreen({threshold: '0.2'});
 
     const articlesList = [
-        { title: 'Which Text Case to Use', desc: 'Ever wondered which text case to use ? A little thing like capitalization...', img: Image, src: 'https://medium.com/@diaslalinda/which-text-case-to-use-d583b2ad2006' },
-        { title: 'UX for Call Center Applications', desc: 'Recently I wanted to travel to USA. I decided to book flights...', img: Image, src: 'https://medium.com/@diaslalinda/ux-for-call-center-applications-7b47116b6862' },
-        { title: 'Managing Large UI Development Projects', desc: 'Usually people thinks that CSS or SASS can be written in any way.', img: Image, src: 'https://medium.com/@diaslalinda/managing-large-ui-development-projects-61a7166834f7' },
-        { title: 'Media Queries in Responsive Web Design', desc: 'For those who wonder what media queries is, media query...', img: Image, src: 'https://medium.com/@diaslalinda/media-queries-in-responsive-web-design-51bfb9fcc9ff' }
+        { title: 'Which Text Case to Use', desc: 'Ever wondered which text case to use ? A little thing like capitalization...', img: Article1, src: 'https://medium.com/@diaslalinda/which-text-case-to-use-d583b2ad2006' },
+        { title: 'UX for Call Center Applications', desc: 'Recently I wanted to travel to USA. I decided to book flights...', img: Article2, src: 'https://medium.com/@diaslalinda/ux-for-call-center-applications-7b47116b6862' },
+        { title: 'Managing Large UI Development Projects', desc: 'Usually people thinks that CSS or SASS can be written in any way.', img: Article2, src: 'https://medium.com/@diaslalinda/managing-large-ui-development-projects-61a7166834f7' },
+        { title: 'Media Queries in Responsive Web Design', desc: 'For those who wonder what media queries is, media query...', img: Article2, src: 'https://medium.com/@diaslalinda/media-queries-in-responsive-web-design-51bfb9fcc9ff' }
     ];
 
     function ShowArticles(props) {
